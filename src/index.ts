38770a6727ea3f5358899684b00de9fc7a34902c
olmedo <olmedo,bdp@gmail.com>
2020-06-27 23:50:17 -0500

    separo configuracion de index

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,32 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import router from './routes/routes';
 import { createConnection} from 'typeorm';
 import 'reflect-metadata';
+import app from './app'
+import router from './routes/routes';
+import { Request, Response, NextFunction} from 'express'
 
-const app = express()
-createConnection();
 
-//middlewares
-app.use(cors());
-app.use(morgan('dev'));
-app.use(express.json());
+let port = 3900;
+
+
 
 //routes 
-app.use(router);
+app.use('/api/v1', router);
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).send("Ruta no encontrada!");
+});
 
-app.listen(3000);
-console.log('server on port', 3000);
+
+
+createConnection()
+    .then( async connection =>{
+
+        console.log("Conectado a la Base de datos")
+    })
+    .catch(error => console.log("TypeORM error en conexion a la BD: ", error));
+
+
+
+app.listen(port, () => {
+
+    console.log('Servidor escuchando en http://localhost:'+port);
+});
